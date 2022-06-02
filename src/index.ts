a088fbb4e1d2018ef9b54d123daf5f6e28801fb8
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2022-06-02 18:58:42 +0300

    implemented simple pub sub

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+type Identifier = string;
+type Unregister = () => void;
+interface Listener {
+  name: string;
+  action: EventListener;
+}
+
+const listeners = new Map<Identifier, Listener>();
+
+function generateUID(prefix = 'UID'): Identifier {
+    return `${prefix}-${(~~(Math.random() * 1e8)).toString(16)}`;
+}
+
+const unregister = (id: Identifier) => {
+    if (listeners.has(id)) {
+        const { name, action } = listeners.get(id) as Listener;
+        window.removeEventListener(name, action);
+        listeners.delete(id);
+    }
 };
+
+const register = (name: string, action: EventListener): Unregister => {
+    const id = generateUID(name);
+    listeners.set(id, { name, action });
+    window.addEventListener(name, action);
+    
+    return unregister.bind(this, id);
+};
+
+const emit = <T extends unknown>(name: string, data: T) => {
+    window.dispatchEvent(new CustomEvent(name, { detail: data }))
+};
+
+const clearAll = () => {
+    listeners.clear();
+};
+
+export { emit, register, clearAll };
